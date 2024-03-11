3b620678d0263ab7a10834d2bf260280724b0efd
Avisheik Barua <avisheikbarua960@gmail.com>
2024-03-11 16:58:11 +0600

    first commit

---
+++ src/components/setupProxy.js
@@ -0,0 +1,12 @@
+// src/setupProxy.js
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// module.exports = function (app) {
+//   app.use(
+//     "/api", // Adjust the path according to your API routes
+//     createProxyMiddleware({
+//       target: "https://contact.mediusware.com",
+//       changeOrigin: true,
+//     })
+//   );
+// };
